6fd46db47d461c2cdd4b66e32f0a0850acaa03a0
sarahsaleh <sarahtsige@gmail.com>
2020-02-07 09:45:14 -0500

    created divs for tab content

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 
 
-const url = "https://www.anapioficeandfire.com/api/houses"
-
+const url = "https://www.anapioficeandfire.com/api/houses?page=2&pageSize=50";
+const tabs = documents.querySelectorAll('data-tab-target')
 
 fetch(url)
   .then(res => {
     return res.json();
   })
   .then(res => {
-    console.log("success!", res);
+    console.log("success   res");
+    console.log(res);
   })
   .catch(err => {
     console.log("something went wrong...", err);
@@ -19,23 +20,25 @@ fetch(url)
 
 
 let houses = document.querySelectorAll('.houses')
+houses.innerHTML = res[0].name;
 
-
-
-
-
-//Event listener for the click
-houses.addEventListener('click', getHouseInfo);
-
-
-function getHouseInfo(e){
-    e.preventDefaut();
+function getHouseInfo(e) {
+  e.preventDefaut();
+  const target = document.querySelector(tab.dataset.tabTarget)
+  target.classList.add('active')
 }
+
+//Event listener to tabs for the click
+tabs.forEach(tab => {
+  tab.addEventListener('click', getHouseInfo);
+})
+
 
 
 
 
 //array of URLs for images
- let houseImages {
+let houseImages = {};
 
- }
+
+
