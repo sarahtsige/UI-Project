a4bd99626873c4957190e2d690e31d6b8126b415
sarahsaleh <sarahtsige@gmail.com>
2020-02-07 16:12:54 -0500

    mofiied css

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 
 
-const url = "https://www.anapioficeandfire.com/api/houses?page=2&pageSize=50";
-const tabs = documents.querySelectorAll('data-tab-target')
+let baseUrl = "ttps://anapioficeandfire.com/api/houses/";
+let tabs = document.querySelectorAll('[data-tab-target]')
+let tabContents = document.querySelectorAll('[data-tab-content]')
+let houses = document.querySelectorAll('.houses')
+let tab
 
-fetch(url)
+
+//array of URLs for images
+let housesNumbers = {
+  stark:'362',
+  targaryen:'378',
+  lannister:'229',
+  baratheon:'15',
+  tyrel:'398',
+};
+
+housesNumbers2 = [362, 378, 229, 15, 398]
+
+
+for (let i = 0; i <housesNumbers2.length; i++) {
+fetch(baseUrl+housesNumbers[0])
   .then(res => {
     return res.json();
   })
@@ -14,39 +31,37 @@ fetch(url)
   .catch(err => {
     console.log("something went wrong...", err);
   });
-
-
-
-
-
-let houses = document.querySelectorAll('.houses')
-houses.innerHTML = res[0].name;
-
-
-
-function getHouseInfo(e) {
-  e.preventDefaut();
-  const target = document.querySelector(tab.dataset.tabTarget)
-  target.classList.add('active')
+  //houses.innerHTML = res[0].name;
 }
+
+
+
+
+//
+
+
+
+// function getHouseInfo(e) {
+//   e.preventDefaut();
+//   const target = document.querySelector(tab.dataset.tabTarget)
+//   target.classList.add('active')
+// }
 
 //Event listener to tabs for the click
 tabs.forEach(tab => {
-  tab.addEventListener('click', getHouseInfo);
+  tab.addEventListener('click', myFunction() => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    target.classList.add('active')
+  })
 })
 
 
 
 
 
-//array of URLs for images
-let houseImages = {
-  stark:'https://wallpaperboat.com/wp-content/uploads/2019/04/game-of-thrones-sigil-gerb-wallpaper-hd-picture-032.jpg.jpg',
-  targaryen:'https://wallpaperboat.com/wp-content/uploads/2019/04/game-of-thrones-sigil-gerb-wallpaper-hd-picture-006.jpg',
-  lannister:'https://wallpaperboat.com/wp-content/uploads/2019/04/game-of-thrones-sigil-gerb-wallpaper-hd-picture-014.jpg',
-  baratheon:'https://wallpaperboat.com/wp-content/uploads/2019/04/game-of-thrones-sigil-gerb-wallpaper-hd-picture-023.jpg',
-  tyrel:'',
-};
 
 
 
