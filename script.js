ed6b24cb1922cc271992ed0b3f1fbaa88f0cd0ad
sarahsaleh <sarahtsige@gmail.com>
2020-02-08 14:40:50 -0500

    pulled in name

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 
 
-let baseUrl = "ttps://anapioficeandfire.com/api/houses/";
-let tabs = document.querySelectorAll('[data-tab-target]')
-let tabContents = document.querySelectorAll('[data-tab-content]')
-let houses = document.querySelectorAll('.houses')
-let tab
+let baseUrl = "https://anapioficeandfire.com/api/houses/";
+let tabs = document.querySelectorAll('[data-tab-target]');
+let tabContents = document.querySelectorAll('[data-tab-content]');
+let houses = document.querySelectorAll('.houses');
+let stark = document.querySelector('.housestark');
+let targaryen = document.querySelector('.housetargaryen');
+let starkTab = document.getElementById('stark');
+let targTab = document.getElementById('targaryen');
+let lanTab = document.getElementById('lannister');
+let barTab = document.getElementById('baratheon');
+let tyrelTab = document.getElementById('tyrel');
+
+
+
+
+
+
+
 
 
 //array of URLs for images
@@ -18,21 +31,76 @@ let housesNumbers = {
 
 housesNumbers2 = [362, 378, 229, 15, 398]
 
+//add event listener for stark tab
 
-for (let i = 0; i <housesNumbers2.length; i++) {
-fetch(baseUrl+housesNumbers[0])
-  .then(res => {
-    return res.json();
+starkTab.addEventListener('click', function () {
+  //test event listener with message to console
+  console.log('stark el worked!')
+  //generate url for each house
+  let houseUrl = baseUrl+housesNumbers2[0];
+  console.log(houseUrl);
+  //use generated url to get house info
+  // fetch(houseUrl).then(res => {
+  //   return res.json();
   })
-  .then(res => {
-    console.log("success   res");
-    console.log(res);
-  })
-  .catch(err => {
-    console.log("something went wrong...", err);
-  });
-  //houses.innerHTML = res[0].name;
-}
+  //capture house name
+  // fetch("https://anapioficeandfire.com/api/houses/362")
+  // .then(res => res.json())
+  // .then (data => console.log(data.name))
+  fetch("https://anapioficeandfire.com/api/houses/362")
+  .then(res => res.json())
+  .then (data => document.querySelector('.name').innerHTML = "Name:    "+data.name);
+
+ // })
+//})
+
+
+
+
+targTab.addEventListener('click', function () {
+  console.log('targ el worked!')
+})
+
+lanTab.addEventListener('click', function () {
+  console.log('lan el worked!')
+})
+
+barTab.addEventListener('click', function () {
+  console.log('bar el worked!')
+})
+
+tyrelTab.addEventListener('click', function () {
+  console.log('tyrel el worked!')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let i = 0; i <housesNumbers2.length; i++) {
+// fetch(baseUrl+housesNumbers2[0])
+//   .then(res => {
+//     return res.json();
+//   })
+//   .then(res => {
+//     console.log("success   res");
+//     console.log(res);
+//   })
+//   .catch(err => {
+//     console.log("something went wrong...", err);
+//   });
+//   //houses.innerHTML = res[0].name;
+// }
 
 
 
@@ -48,15 +116,15 @@ fetch(baseUrl+housesNumbers[0])
 // }
 
 //Event listener to tabs for the click
-tabs.forEach(tab => {
-  tab.addEventListener('click', myFunction() => {
-    const target = document.querySelector(tab.dataset.tabTarget)
-    tabContents.forEach(tabContent => {
-      tabContent.classList.remove('active')
-    })
-    target.classList.add('active')
-  })
-})
+// tabs.forEach(tab => {
+//   tab.addEventListener('click', myFunction() => {
+//     const target = document.querySelector(tab.dataset.tabTarget)
+//     tabContents.forEach(tabContent => {
+//       tabContent.classList.remove('active')
+//     })
+//     target.classList.add('active')
+//   })
+// })
 
 
 
